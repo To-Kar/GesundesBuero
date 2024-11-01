b9444e1ac06e52518048d4dc0e199fc2311fdd32
Alican19 <alsoit00@hs-esslingen.de>
2024-11-01 14:22:40 +0100

    Deployment Test für Azure

---
+++ frontend/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+    <div id="app">
+      <h1>Deployment Test</h1>
+      <p>Wenn du das siehst ist alles in Butter!</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: "App",
+  };
+  </script>
+  
