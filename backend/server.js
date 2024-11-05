4c57fc8509163d9a5a882dcfb832628c29909b55
Alican19 <alsoit00@hs-esslingen.de>
2024-11-05 16:16:30 +0100

    some changes for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, '../frontend/dist')));
+// Bereitstellung der statischen Dateien aus dem dist-Ordner
+app.use(express.static(path.join(__dirname, './frontend/dist')));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+// Leite alle Anfragen auf index.html weiter, um das clientseitige Routing zu unterstützen
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './frontend/dist/index.html'));
 });
 
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server läuft auf Port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
