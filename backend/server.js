db6cbcc5162a6a88c337f217629dc449352d0e34
Alican19 <alsoit00@hs-esslingen.de>
2024-11-05 16:22:36 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,11 @@ const path = require('path');
 const app = express();
 
 // Bereitstellung der statischen Dateien aus dem dist-Ordner
-app.use(express.static(path.join(__dirname, './frontend/dist')));
+app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
 // Leite alle Anfragen auf index.html weiter, um das clientseitige Routing zu unterstützen
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './frontend/dist/index.html'));
+    res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
 });
 
 const PORT = process.env.PORT || 3000;
