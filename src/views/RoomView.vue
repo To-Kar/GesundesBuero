f8aadd5cfc84f1105cfa94b2eca936eb0ee95a1b
arturhoxha <artur.hoxha@hotmail.de>
2024-11-20 20:34:42 +0100

    4 weitere Räume wurden hinzugefügt

--- src/views/RoomView.vue
+++ src/views/RoomView.vue
@@ -10,7 +10,27 @@ export default {
 </script>
 
 <template>
+  <div class="room-view">
+    <Room />
+    <Room />
+    <Room />
+    <Room />
+    <Room />
+  </div>  
 
-  <Room />
-  
 </template>
+
+<style scoped>  
+.room-view {
+  display: flex; 
+  flex-wrap: wrap; 
+  justify-content: center;
+  gap: 1rem; 
+}
+
+.room-view > * {
+  flex: 1 1 calc(50% - 1rem); 
+  max-width: calc(50% - 1rem); 
+  box-sizing: border-box; 
+}
+</style>
