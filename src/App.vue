2b6c7e4e0a1903b4dc6f6cff0f818cbcd0fd5b79
arturhoxha <artur.hoxha@hotmail.de>
2024-11-20 16:46:32 +0100

    Update Room.vue
    Header wurde entfernt

--- src/App.vue
+++ src/App.vue
@@ -10,39 +10,9 @@ export default {
 
 <template>
   <div>
-    <header class="app-header">
-      <img src="./assets/Systec.jpg" alt="Header" class="header-image" />
-    </header>
+    
     <div class="app-content">
       <Room />
     </div>
   </div>
 </template>
-
-<style>
-/* Styling für den Header */
-.app-header {
-  width: 100%;
-  height: auto;
-  background-color: #ffffff;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  border-bottom: 2px solid black;
-}
-
-.header-image {
-  width: 100%;
-  max-width: 1200px; /* Maximale Breite */
-  height: auto; /* Erhalte Seitenverhältnis */
-}
-
-/* Styling für den App-Inhalt */
-.app-content {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: calc(100vh - 100px); /* Höhe des Inhalts abzüglich Header-Höhe */
-  background-color: #eaeaea;
-}
-</style>
