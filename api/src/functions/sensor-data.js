467b396c7ac70e8d5b38ea34c78bb63baca13099
KevinBoehmisch <kevinboehmisch2049@gmail.com>
2024-11-20 21:01:01 +0100

    API für Rooms Sensordaten hinzugefügt.References #34

---
+++ api/src/functions/sensor-data.js
@@ -0,0 +1,61 @@
+const { app } = require('@azure/functions');
+
+// Mock-Datenstruktur für Räume mit sensor-data Key-Value Pair (später echte Daten aus Datenbank)
+const rooms = {
+    "room1": {
+        sensorData: {
+            temperature: 22.5,
+            humidity: 45.0
+        }
+    },
+    "room2": {
+        sensorData: {
+            temperature: 22.5,
+            humidity: 45.0
+        }
+    },
+    "room3": {
+        sensorData: {
+            temperature: 22.5,
+            humidity: 45.0
+        }
+    },
+    "room4": {
+        sensorData: {
+            temperature: 22.5,
+            humidity: 45.0
+        }
+    },
+    "room5": {
+        sensorData: {
+            temperature: 22.5,
+            humidity: 45.0
+        }
+    }
+};
+
+app.http('sensor-data', {
+    methods: ['GET'],
+    authLevel: 'anonymous',
+    route: 'rooms/{roomId}/sensor-data', // Route definieren
+    handler: async (request, context) => {
+        context.log(`Http function processed request for url "${request.url}"`);
+        const roomId = request.params.roomId; // roomId aus der URL
+
+        // überprüfen ob Raum vorhanden
+        if (!rooms[roomId]) {
+            context.log(`Room with ID ${roomId} not found`); // Anzeige auf Terminal
+            return {
+                body: { error: `Room with ID ${roomId} not found` }
+            };
+        }
+        // Daten zurückgeben
+        context.log(`Sensor data for ${roomId}:`, rooms[roomId].sensorData); //Anzeige auf Terminal
+        return {
+            body: rooms[roomId].sensorData
+        };
+    }
+});
+
+
+
