7282b06a67490347b42407217bb317b6a25de897
MarvinB1012 <Berishamarvin@gmail.com>
2024-11-26 15:28:46 +0100

    router erweitert und fertiggestellt

--- src/router/index.js
+++ src/router/index.js
@@ -1,1 +1,12 @@
 import { createRouter, createWebHistory} from 'vue-router';
+
+const router = createRouter({
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+        },
+    ],
+}
+);
+        
+export default router;
