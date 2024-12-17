bdf2117a7991b7a0e43e8761e9bb6074b014b7ca
KevinBoehmisch <kevinboehmisch2049@gmail.com>
2024-12-17 14:49:56 +0100

    hinzufügen in RoomView, dass Temperatur und Humidity in Intervall (update_interval) aktualisiert werden ohne refresh der Seite

--- src/services/settingsService.js
+++ src/services/settingsService.js
@@ -6,7 +6,19 @@ export const settingsService = {
       const response = await apiClient.get('/settings');
       return response.data;
     } catch (error) {
-      console.error('Fehler beim Abrufen der Einstellungen:', error);
+      console.error('Fehler beim Abrufen der Einstellungen:', error.message);
+      throw error;
+    }
+  },
+
+  async fetchAndReturnInterval() {
+    try {
+      const response = await this.getSettings();
+      const updateInterval = response.update_interval * 1000; // Sekunden zu Millisekunden
+      console.log("Update-Intervall geladen:", updateInterval, "ms");
+      return updateInterval;
+    } catch (error) {
+      console.error("Fehler beim Laden des Update-Intervalls:", error.message);
       throw error;
     }
   },
