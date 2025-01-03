0f164f0da63bc99c33df2eec14266a191fa398a6
KevinBoehmisch <kevinboehmisch2049@gmail.com>
2025-01-04 00:53:52 +0100

    test

--- api/src/functions/app.js
+++ api/src/functions/app.js
@@ -1,13 +1,8 @@
 // app.js
 
 const { app } = require('@azure/functions');
-const validateJwt = require('./utils/validateJwt');
 
 // Funktionen importieren
 
-require('./controllers/sensorController');
-require('./controllers/notificationController');
-require('./controllers/settingsController');
-require('./controllers/roomController');
-require('./controllers/historyController');
-require('./functions/httpTrigger1');
+
+require('./httpTrigger1');
