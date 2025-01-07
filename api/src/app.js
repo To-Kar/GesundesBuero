24121c31f61fb763b249c68a03852607e6420470
arturhoxha <artur.hoxha@hotmail.de>
2025-01-07 01:31:41 +0100

    hotfix

--- api/src/app.js
+++ api/src/app.js
@@ -2,7 +2,7 @@ const { app } = require('@azure/functions');
 
 require('./controllers/historyController');
 require('./controllers/notificationController')
-require('./controllers/roomController')
+require('./controllers/roomController');
 require('./controllers/sensorController');
-require('./controllers/settingsController')
+require('./controllers/settingsController');
 require('./swagger/swaggerRoutes');
